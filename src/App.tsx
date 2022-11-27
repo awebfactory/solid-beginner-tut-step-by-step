df684259e97a1b02362f4c1a165ce51015839b55
Victor Kane <victorkane@gmail.com>
2022-11-27 12:44:06 -0300

    Building UI with Components: Composing multiple components together

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import type { Component } from "solid-js"
 import { HellowWorld } from "./HelloWorld"
+import { BookList } from "./BookList"
+import { AddBook } from "./AddBook"
 
-import logo from "./logo.svg"
-import styles from "./App.module.css"
+interface BookshelfProps {
+  name: string
+}
+
+function Bookshelf(props: BookshelfProps) {
+  return (
+    <div>
+      <h2>{props.name}'s Bookshelf</h2>
+      <BookList />
+      <AddBook />
+    </div>
+  )
+}
 
 const App: Component = () => {
   return (
     <div style="padding: 1rem">
       <h1>Welcome</h1>
       <HellowWorld />
+      <Bookshelf name="Solid" />
     </div>
   )
 }
